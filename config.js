7ac7c372b95a9fe16036da9277b0af0aa7f9fee0
tedster-08 <75181949+tedster-08@users.noreply.github.com>
2022-03-05 18:51:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
+  name: 'Teddy',
   imageBackground: false,
   openInNewTab: true,
   twelveHourFormat: false,
@@ -28,7 +28,7 @@ const CONFIG = {
   // Weather
   weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
   weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-  weatherUnit: 'C', // 'F', 'C'
+  weatherUnit: 'F', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
